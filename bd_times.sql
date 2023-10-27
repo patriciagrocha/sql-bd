dcadaf8a5f17b6e615a1ff8d239242239f824a9a
Patricia Rocha <patirocha.dev@gmail.com>
2023-10-26 22:12:24 -0300

    novo campo de grupos

--- bd_times.sql
+++ bd_times.sql
@@ -46,3 +46,16 @@ SELECT * FROM times WHERE nome like 'E%';
 SELECT * FROM times WHERE LOWER(nome) like 'e%';
 SELECT * FROM times WHERE LOWER(nome) like 'e%' AND sigla like 'U%';
 
+ALTER TABLE times ADD COLUMN grupo varchar(1);
+
+UPDATE times SET grupo = 'A' WHERE nome IN ('Qatar', 'Equador', 'Senegal', 'Holanda');
+UPDATE times SET grupo = 'B' WHERE nome IN ('Inglaterra', 'Irã', 'Estados Unidos', 'País de Gales');
+UPDATE times SET grupo = 'C' WHERE nome IN ('Argentina', 'Arábia Saudita', 'México', 'Polônia');
+UPDATE times SET grupo = 'D' WHERE nome IN ('França', 'Dinamarca', 'Tunísia', 'Austrália');
+UPDATE times SET grupo = 'E' WHERE nome IN ('Espanha', 'Alemanha', 'Japão', 'Costa Rica');
+UPDATE times SET grupo = 'F' WHERE nome IN ('Bélgica', 'Canadá', 'Marrocos', 'Croácia');
+UPDATE times SET grupo = 'G' WHERE nome IN ('Brasil', 'Sérvia', 'Suíça', 'Camarões');
+UPDATE times SET grupo = 'H' WHERE nome IN ('Portugal', 'Gana', 'Uruguai', 'Coréia do Sul');
+commit;
+
+SELECT * FROM times;
